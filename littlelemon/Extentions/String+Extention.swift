3bae115344f678512ec4c4aad41a0ac6c428dcf9
ali <shaukat.mc@gmail.com>
2023-10-24 17:19:33 +0200

    add user onboarding flow

---
+++ littlelemon/Extentions/String+Extention.swift
@@ -0,0 +1,8 @@
+//
+//  String+Extention.swift
+//  littlelemon
+//
+//  Created by Ali on 24/10/23.
+//
+
+import Foundation
---
+++ littlelemon/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  littlelemon
+//
+//  Created by Ali on 24/10/23.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
