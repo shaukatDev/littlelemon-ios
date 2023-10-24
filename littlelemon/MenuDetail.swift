18cd4985b568f402fe5ae5376c24684b785553e1
ali <shaukat.mc@gmail.com>
2023-10-24 21:23:58 +0200

    Fetch data from server and siplay it

---
+++ littlelemon/MenuDetail.swift
@@ -0,0 +1,18 @@
+//
+//  MenuDetail.swift
+//  littlelemon
+//
+//  Created by Ali on 24/10/23.
+//
+
+import SwiftUI
+
+struct MenuDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuDetail()
+}
